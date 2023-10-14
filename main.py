9435fdbba996776d12d3ea142aa1b8e60b3b3b02
MohammadrezaAmani <more.amani@yahoo.com>
2023-10-14 05:35:31 +0330

    update code style

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from autinfo import AutInfo
 
-user = AutInfo(username="<__USERNAME__>", password="<__PASSWORD__>")
+user = AutInfo(
+    username="<__USERNAME__>", 
+    password="<__PASSWORD__>"
+)
 
 print("GET SPECIAL USER OR SMALL RANGE:".center(50, "*"))
 print(user.get(9913004))
