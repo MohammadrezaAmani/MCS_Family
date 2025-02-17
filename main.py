e4bfce0d48d21f0aec1d04d3b8416f144a6d7f3c
Mohammadreza Amani <More.Amani@yahoo.com>
2025-02-17 16:39:10 +0330

    add async example.

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from autinfo import AutInfo
+import asyncio
 
-user = AutInfo(
-    username="<__USERNAME__>", 
-    password="<__PASSWORD__>"
-)
+from autinfo import AutInfoAsync, AutInfoSync
 
-print("GET SPECIAL USER OR SMALL RANGE:".center(50, "*"))
-print(user.get(9913004))
+username, password = "9913004", "0820494501"
 
 
-print("GET A RANGE".center(50, "*"))
-print(user.get_range(40013004, 40013008))
+async def main_async():
+    async with AutInfoAsync(username, password) as client:
+        student_data = await client.get(9913004)
+        print("Student Data:", student_data)
+
+        student_ids = await client.get_range(9813004, 9813006)
+        print("Student IDs in range:", student_ids)
+
+
+def main_sync():
+    client = AutInfoSync(username, password)
+    student_data_sync = client.get(9913004)
+    print("[Sync] Student Data:", student_data_sync)
+
+    student_ids_sync = client.get_range(40013004, 40013006)
+    print("[Sync] Student IDs in range:", student_ids_sync)
+
+
+if __name__ == "__main__":
+    asyncio.run(main_async())
+    main_sync()
